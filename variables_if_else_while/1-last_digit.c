c63e54ebc8f36f2eb2dfeeaf822a5a498188f0b6
Sasct <freyderbejaranoduvan@gmail.com>
2024-10-23 17:24:26 -0500

    tasks 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,7 +2,6 @@
 #include <time.h>
 #include <stdio.h>
 
-
 /**
  * main - prints a text according number
  *
@@ -21,5 +20,6 @@ int main(void)
 		printf("last digit of %d is %d and is 0\n", n, n % 10);
 	else
 		printf("last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+
 	return (0);
 }
